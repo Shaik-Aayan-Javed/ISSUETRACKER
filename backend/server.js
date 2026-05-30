3f55e891d70e77166426a471be01a43f8227b89f
Pranathi Balusu <bit.pranathi@gmail.com>
2026-05-30 14:40:02 +0530

    Fix deployment port

--- backend/server.js
+++ backend/server.js
@@ -65,9 +65,11 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ Connected to MongoDB');
-    app.listen(process.env.PORT, () =>
-      console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
-    );
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
   })
   .catch((err) => {
     console.error('❌ MongoDB error:', err.message);
